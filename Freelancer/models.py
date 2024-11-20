289d2c2120d3e4845bd351f7a6880b29918b117b
Zakar1234iya <zakari1234a@gmail.com>
2024-11-20 13:28:50 +0200

    new

--- Freelancer/models.py
+++ Freelancer/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Freelancer(models.Model):
+    fname = models.CharField(max_length=50)
+    lname = models.CharField(max_length=50)
+    email = models.EmailField()
+    phone_number = models.SmallIntegerField(max_length=10)
+    password = models.CharField(max_length=254)
+    
+    
--- Muhtaraif/settings.py
+++ Muhtaraif/settings.py
@@ -73,10 +73,15 @@ WSGI_APPLICATION = 'Muhtaraif.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'mansaf',   # Replace with your MySQL database name
+        'USER': 'prj_mansaf',   # Replace with your MySQL username
+        'PASSWORD': '@toor369',  # Replace with your MySQL password
+        'HOST': '127.0.0.1',   # Or the address of your MySQL server
+        'PORT': '3306',        # Default MySQL port
     }
 }
 
@@ -121,3 +126,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
