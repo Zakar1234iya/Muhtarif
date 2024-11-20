0d24ac388789211bf1c775680a45404db3fbef6d
Zakaria Jibreen <zakari1234a@gmail.com>
2024-11-20 18:06:52 +0200

    classes update

--- address/models.py
+++ address/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Address(models.Model):
+    address_id = models.SmallIntegerField()
+    address_name = models.CharField(max_length=50)
+
+
+def add_address():
+    Address
