dc07bfd64990d40482e1aca1dd0cf0818baefb9a
Zakaria Jibreen <zakari1234a@gmail.com>
2024-11-21 11:25:25 +0200

    add forgin key

--- Freelancer/models.py
+++ Freelancer/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from address.models import Address
 
 class Freelancer(models.Model):
     fname = models.CharField(max_length=50)
@@ -8,4 +9,4 @@ class Freelancer(models.Model):
     password = models.CharField(max_length=254)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    
+    address = models.ForeignKey(Address)
