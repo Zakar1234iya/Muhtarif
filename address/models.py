b918784e5f99bfb2ab270b0ad83298337cb8e281
Zakaria Jibreen <zakari1234a@gmail.com>
2024-11-21 13:30:45 +0200

    update models

--- address/models.py
+++ address/models.py
@@ -8,10 +8,10 @@ class Address(models.Model):
 def add_address():
     addresses = [
         {"address_id": 1, "address_name": "جنين"},
-        {"address_id": 2, "address_name": "قلقيلية"},
+        {"address_id": 2, "address_name": "طوباس"},
         {"address_id": 3, "address_name": "تابلس"},
         {"address_id": 4, "address_name": "طولكرم"},
-        {"address_id": 5, "address_name": "جنين"},
+        {"address_id": 5, "address_name": "قلقيلية"},
         {"address_id": 6, "address_name": "سلفيت"},
         {"address_id": 7, "address_name": "رام الله"},
         {"address_id": 8, "address_name": "أريحا"},
