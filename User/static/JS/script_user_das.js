2571df0c6df5990052f4a002cecc0f1c32412e61
Ali-Bh-yahya <zadanz.112233@gmail.com>
2024-11-22 14:38:00 +0200

    edit in user dashboard
    add java script file and edit in css

---
+++ User/static/JS/script_user_das.js
@@ -0,0 +1,17 @@
+const profilePic = document.getElementById('profile-pic');
+        const iconsContainer = document.getElementById('icons-container');
+ 
+        profilePic.addEventListener('click', () => {
+            if (iconsContainer.style.display === 'flex') {
+                iconsContainer.style.display = 'none';
+            } else {
+                iconsContainer.style.display = 'flex';
+            }
+        });
+ 
+ 
+        window.addEventListener('click', (e) => {
+            if (!profilePic.contains(e.target) && !iconsContainer.contains(e.target)) {
+                iconsContainer.style.display = 'none';
+            }
+        });
