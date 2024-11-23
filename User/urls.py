dc9dfb4f18002e1f02ce9373f7051c0543912d08
Zakaria Jibreen <zakari1234a@gmail.com>
2024-11-23 19:05:48 +0200

    get everything together
    update usrls file in total

--- User/urls.py
+++ User/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.user_dashboard),
-    path('profile', views.user_profile , name="user_profile")
+    path('profile', views.user_profile , name="user_profile"),
     path('chat', views.user_profile , name="chat")
 
 ]
