ae2eb94c2949f3e0bc8b98100c70326904750dc4
Zakaria Jibreen <zakari1234a@gmail.com>
2024-11-24 14:58:53 +0200

    .
    .

--- Freelancer/urls.py
+++ Freelancer/urls.py
@@ -3,15 +3,19 @@ from . import views
 from django.conf import settings
 from django.conf.urls.static import static
 
+<<<<<<< Updated upstream
 from django.urls import path
 from . import views
+=======
+>>>>>>> Stashed changes
 
 urlpatterns = [
 <<<<<<< Updated upstream
     path('', views.index, name='freelancer_dashboard'),
     path('fetch/', views.fetch_freelancers, name='fetch-freelancers'),
-    path('profile/<int:freelancer_id>/', views.freelancer_profile, name='freelancer-profile'),
+    path('profile/<int:freelancer_id>/', views.viewfreelancer, name='profile'),
     path('edit_profile/<int:freelancer_id>/', views.update_freelancer_profile, name='edit-freelancer-profile'),
+<<<<<<< Updated upstream
     path('post/', views.freelancer_post, name='freelancer_post'),
 =======
     path('', views.index, name='freelancer_dashboard'),  # Dashboard for freelancers
@@ -20,5 +24,9 @@ urlpatterns = [
     path('edit_profile/<int:freelancer_id>/', views.update_freelancer_profile, name='edit-freelancer-profile'),  # Edit freelancer profile
     path('viewfreelancer/<int:freelancer_id>/', views.viewfreelancer, name='viewfreelancer'),  # View freelancer profile with comments
     path('post/', views.freelancer_post, name='freelancer_post'),  # Freelancer post creation and comment handling
+>>>>>>> Stashed changes
+=======
+    # path('post/', views.freelancer_post, name='freelancer_post'),
+    path('viewfreelancer/<int:freelancer_id>/', views.viewfreelancer, name='viewfreelancer'),
 >>>>>>> Stashed changes
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
